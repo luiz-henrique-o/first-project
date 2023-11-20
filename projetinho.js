d2a1680e88553465deebeca4349c50ee956590fb
luiz-henrique-o <luizhenriquedeoliveirasilva61@gmail.com>
2023-11-20 17:51:32 -0300

    my first commit

---
+++ projetinho.js
@@ -0,0 +1,17 @@
+const cadastrado = "Cadastrado com Sucesso!" 
+let timePokemon = [
+  ["Poochyena", "M", 2], 
+  ["Zigzagoon, "F", 2],
+  ["Dragonite", "M", 5],
+  ["Dragonite", "F", 5],
+  ["Dragonite", "F", 5],
+  ["Poochyena", "F", 3], 
+  ["Wurmple", "M", 2]
+] 
+console.log("Poochyena " + cadastrado)
+console.log("Zigzagoon " + cadastrado)
+console.log("Dragonite " + cadastrado)
+console.log("Dragonite " + cadastrado)
+console.log("Dragonite " + cadastrado)
+console.log("Poochyena " + cadastrado)
+console.log("Wurmple " + cadastrado)
